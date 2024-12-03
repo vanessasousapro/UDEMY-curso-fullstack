7a41bb619904ee48d02b961813ace0397594a2fd
vanessasousapro <vanessasousa.pro@gmail.com>
2024-12-03 10:19:11 -0300

    Ajustes No Código

--- javascript/Sessão_2_Js_Básico/exercicio_funcao_objeto_array/js/script.js
+++ javascript/Sessão_2_Js_Básico/exercicio_funcao_objeto_array/js/script.js
@@ -1,10 +1,10 @@
-function meuEscopo () {
+function meuEscopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
-    
+
     const pessoas = [];
 
-    function recebeEventoForm (evento) {
+    function recebeEventoForm(evento) {
         evento.preventDefault()
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
@@ -12,10 +12,10 @@ function meuEscopo () {
         const altura = form.querySelector('.altura');
 
         pessoas.push({
-            nome : nome.value,
-            sobrenome : sobrenome.value,
-            peso : peso.value,
-            altura : altura.value
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
         });
 
         console.log(pessoas);
